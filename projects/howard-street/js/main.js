b6af9612d92e6ce4f71db648f527c85ae7fe7d0c
addahd <adda1806@gmail.com>
2024-05-29 12:56:26 +0100

    Ver 0.1.016

--- projects/howard-street/js/main.js
+++ projects/howard-street/js/main.js
@@ -1,7 +1,7 @@
 function myFuntion() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
-    x.className += "responsive";
+    x.className += " responsive";
   } else {
     x.className = "topnav";
   }
